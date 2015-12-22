7cbc1bdfaae183e795cfe8b89884ce02d21b6cdd
Shair Mir <s.mir89@hotmail.com>
2015-12-22 17:15:33 +0100

    New files after refactoring

--- admin/includes/top_nav.php
+++ admin/includes/top_nav.php
@@ -1,11 +1,12 @@
-<div class="navbar-header">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand" href="../index.php">SB Admin</a>
             </div>
 
 
