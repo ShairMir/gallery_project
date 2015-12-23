1b00cf7d8ddfd73a95c12f7a40126deb55b802a8
Shair Mir <s.mir89@hotmail.com>
2015-12-23 21:53:25 +0100

    moved connection to database.php

--- admin/includes/new_config.php
+++ admin/includes/new_config.php
@@ -7,12 +7,5 @@ define('DB_USER','root');
 define('DB_PASS','123');
 define('DB_NAME','gallery_db');
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($connection) {
-	echo "TRUE";
-}
-
-
 
 ?>
