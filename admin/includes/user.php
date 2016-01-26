1af306557c6173f4c22970af6af68dc3976300b8
Shair Mir <s.mir89@hotmail.com>
2016-01-26 11:04:40 +0100

    Updated User Object

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -3,17 +3,58 @@
 
 class User {
 
+	public $id;
+	public $username;
+	public $password;
+	public $first_name;
+	public $last_name;
 
-	public function find_all_users(){
+	public static function find_all_users(){
 		
-		global $database;
-		$result_set = $database->query("SELECT * FROM users");
+		return self::find_this_query("SELECT * FROM users");
 
+	}
+
+	public static function find_user_by_id($user_id){
+
+		global $database;
+
+		$result_set = self::find_this_query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
+		$found_user = mysqli_fetch_array($result_set);
+		return $found_user;
+
+	}
+
+	public static function find_this_query($sql){
+
+		global $database;
+
+		$result_set = $database->query($sql);		
 		return $result_set;
 
 	}
 
+	public static function instantiation($the_record){
 
+		$the_object = new self;
+
+        // $the_object->id 		= $found_user['id'];
+        // $the_object->username 	= $found_user['username'];
+        // $the_object->password 	= $found_user['password'];
+        // $the_object->first_name = $found_user['first_name'];
+        // $the_object->last_name 	= $found_user['last_name'];
+		
+        return $the_object;
+
+	}
+
+	private function has_the_attribute($the_attribute){
+
+		$object_properties = get_object_vars($this);
+
+		return array_key_exists($the_attribute, $object_properties);
+
+	}
 
 
 }
